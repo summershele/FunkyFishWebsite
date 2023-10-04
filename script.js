eb9e717a5c8ff9f0ca66036f248889196a5e71a2
Summer <sashele@ilstu.edu>
2023-10-04 18:48:02 -0500

    slideshow

--- script.js
+++ script.js
@@ -1,43 +1,87 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0; // Start at 0 because we will immediately increment it
+
+ 
 
 // Next/previous controls
+
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+
+  slideIndex += n;
+
+  showSlides();
+
 }
+
+ 
 
 // Thumbnail image controls
+
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+
+  slideIndex = n - 1;  // Subtract 1 to handle zero-based indexing
+
+  showSlides();
+
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-let slideIndex2 = 0;
-showSlides();
+ 
 
 function showSlides() {
-  let i;
+
   let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
+
+  let dots = document.getElementsByClassName("dot");
+
+ 
+
+  // Reset slides and dots
+
+  for (let i = 0; i < slides.length; i++) {
+
     slides[i].style.display = "none";
+
   }
-  slideIndex2++;
-  if (slideIndex2 > slides.length) {slideIndex2 = 1}
-  slides[slideIndex2-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+  for (let i = 0; i < dots.length; i++) {
+
+    dots[i].className = dots[i].className.replace(" active", "");
+
+  }
+
+ 
+
+  // Cycle slide index
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+
+    slideIndex = 1;
+
+  }
+
+ 
+
+  // Show the current slide and set the active dot
+
+  slides[slideIndex-1].style.display = "block";
+
+  if (dots.length > 0) {
+
+    dots[slideIndex-1].className += " active";
+
+  }
+
+ 
+
+  // Schedule the next rotation
+
+  setTimeout(showSlides, 2000);
+
 }
+
+ 
+
+// Start the slideshow
+
+showSlides();
