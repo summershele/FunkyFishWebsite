9f19ed7e71eb35e844acea3fb0059ee207776baa
Sky Wallace <sjwall3@ilstu.edu>
2023-10-09 16:08:31 -0500

    Removed excessive white space and updated comments

--- script.js
+++ script.js
@@ -1,87 +1,49 @@
-let slideIndex = 0; // Start at 0 because we will immediately increment it
-
- 
+let slideIndex = 0;
+let slideTimer; // Store the timer ID
 
 // Next/previous controls
-
 function plusSlides(n) {
-
   slideIndex += n;
-
   showSlides();
-
 }
-
- 
 
 // Thumbnail image controls
-
 function currentSlide(n) {
-
-  slideIndex = n - 1;  // Subtract 1 to handle zero-based indexing
-
+  slideIndex = n - 1;
   showSlides();
-
 }
 
- 
-
 function showSlides() {
-
   let slides = document.getElementsByClassName("mySlides");
-
   let dots = document.getElementsByClassName("dot");
 
- 
-
   // Reset slides and dots
-
   for (let i = 0; i < slides.length; i++) {
-
     slides[i].style.display = "none";
-
   }
 
   for (let i = 0; i < dots.length; i++) {
-
     dots[i].className = dots[i].className.replace(" active", "");
-
   }
-
- 
 
   // Cycle slide index
-
   slideIndex++;
-
   if (slideIndex > slides.length) {
-
     slideIndex = 1;
-
   }
-
- 
 
   // Show the current slide and set the active dot
-
   slides[slideIndex-1].style.display = "block";
-
   if (dots.length > 0) {
-
     dots[slideIndex-1].className += " active";
-
   }
 
- 
+  // Clear any existing timers
+  clearTimeout(slideTimer);
 
   // Schedule the next rotation
-
-  setTimeout(showSlides, 2000);
-
+  slideTimer = setTimeout(showSlides, 2000);
 }
 
- 
-
 // Start the slideshow
-
 showSlides();
