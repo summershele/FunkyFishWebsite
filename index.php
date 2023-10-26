94b62f3e9d898b2f5b9d333e65cf8197c027a990
Sky Wallace <sjwall3@ilstu.edu>
2023-10-25 19:15:52 -0500

    Committing XAMPP stuff

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0;url=FunkyFishWebsite/index.html">
+  <title>Redirecting...</title>
+</head>
+<body>
+  <p>If you are not redirected, <a href="FunkyFishWebsite/index.html">click here</a>.</p>
+</body>
+</html>
+
+
