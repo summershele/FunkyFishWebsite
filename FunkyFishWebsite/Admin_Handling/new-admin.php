2dd5b97cfa9fd4791fd048a995b9818ab7a78888
lkamman18 <144965426+lkamman18@users.noreply.github.com>
2023-11-19 19:00:08 -0600

    Revert "Merge branch 'main' of https://github.com/summershele/FunkyFishWebsite"
    This reverts commit 94128895d4ed04a478f8c6c48dc3e800aa7a7d6d, reversing
    changes made to 142f6fbe8afb9333a3060660d4a33ac67095498b.

--- FunkyFishWebsite/Admin_Handling/new-admin.php
+++ FunkyFishWebsite/Admin_Handling/new-admin.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "admintasks";
+$user = "your_db_user";
+$pass = "your_db_password";
+$dbname = "your_db_name";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
@@ -12,14 +12,14 @@ if ($conn->connect_error) {
 }
 
 // Replace with your desired admin username and password
-$newAdminUsername = "";
-$newAdminPassword = "";
+$newAdminUsername = "desired_username";
+$newAdminPassword = "desired_password";
 
 // Hash the password
 $hashedPassword = password_hash($newAdminPassword, PASSWORD_DEFAULT);
 
 // Prepare and execute the SQL statement
-$sql = "INSERT INTO admin_users (username, password) VALUES (?, ?)";
+$sql = "INSERT INTO admins (username, password) VALUES (?, ?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param('ss', $newAdminUsername, $hashedPassword);
 
